3debf01d7c032b035fcccd0f44ab21f3836e742f
BoxbbBoxbBox <136146679+BoxbbBoxbBox@users.noreply.github.com>
2024-08-24 00:03:12 -0500

    Add calculator skeleton

---
+++ script.js
@@ -0,0 +1,19 @@
+function addition(a, b) {
+  return a + b;
+}
+
+function subtraction(a, b) {
+  return a - b;
+}
+
+function multiplication(a, b) {
+  return a * b;
+}
+
+function division(a, b) {
+  if (b === 0) {
+    return "I don't think so";
+  } else {
+    return a / b;
+  }
+}
