e7f5985e3bf46df38ed010168e48caad2d50d31d
BoxbbBoxbBox <136146679+BoxbbBoxbBox@users.noreply.github.com>
2024-09-01 23:34:29 -0500

    Iterate on button function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let firstNumber = 0;
+let operator = "";
+let secondNumber = 0;
+
 function addition(a, b) {
   return a + b;
 }
@@ -17,3 +21,27 @@ function division(a, b) {
     return a / b;
   }
 }
+
+function operate(operator, firstNumber, secondNumber) {
+  switch (operator) {
+    case "+":
+      return addition(firstNumber, secondNumber);
+    case "-":
+      return subtraction(firstNumber, secondNumber);
+    case "*":
+      return multiplication(firstNumber, secondNumber);
+    case "/":
+      return division(firstNumber, secondNumber);
+    default:
+      return 0;
+  }
+}
+
+// how do i get the below to work on every button?
+
+const element = document.getElementsByClassName("button")[0];
+element.addEventListener("click", function () {
+  let currentDisplay = document.getElementsByClassName("calculatedDisplay");
+  let text = element.innerText;
+  currentDisplay.textContent += text;
+});
